d0354570ac68e274b06ca2c99f444244ec295eb9
perbaw <prabhav.shetty@research.iiit.ac.in>
2025-06-05 06:14:09 +0530

    Chapter 1

---
+++ projects/hello-world/main.rs
@@ -0,0 +1,8 @@
+// this is a comment.
+
+fn main() {
+    println!("Hello world!");       // the ! in println! means we're calling a macro
+}
+
+// rustc main.rs        compiles the program
+// ./main               runs the executable
